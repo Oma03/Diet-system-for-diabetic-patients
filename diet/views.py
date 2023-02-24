e946752a3ee7902d7a86afb434416677a0169b9f
Oma03 <zimnnachioma@gmail.com>
2023-02-24 17:21:53 +0100

    made changes to the html files

--- diet/views.py
+++ diet/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django .contrib.auth import login, logout, authenticate
 # from django.shortcuts import redirect
-from .models import Contact, Details
+from .models import Contact
+
 
 # Create your views here.
 
